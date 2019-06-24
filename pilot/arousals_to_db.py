dda589a0283b22d5c1a338173f4650ef4687efce
vanechev <vanechev@gmail.com>
2019-06-24 11:34:19 +1000

    arousal peaks calculated

--- pilot/arousals_to_db.py
+++ pilot/arousals_to_db.py
@@ -10,7 +10,7 @@ import time
 API_ENDPOINT = "http://localhost:3000/api/v1/visualisations/saveArousalState"
 
 
-file = 'arousals/S2-DR_Peaks.csv'
+file = 'arousals/S4-DR_Peaks.csv'
 df = pd.read_csv(file)
 
 df = df.rename(index=str, columns={"Unnamed: 0": "timestamp"})
@@ -20,7 +20,7 @@ arousal_times = list(df.timestamp)
 print(arousal_times)
 
 for peak in arousal_times:
-	data = {'id_session':'117','person':'280','arousal_time':peak}
+	data = {'id_session':'119','person':'292','arousal_time':peak}
 	r = requests.post(API_ENDPOINT, data = data)
 	print(r.text)
 
